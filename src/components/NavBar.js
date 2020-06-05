ec5b49bf6e7ed3887c6a8f67d7cc5a4710592a12
Title-Jirakul <titlejirakul@gmail.com>
2020-06-04 21:46:37 -0700

    Created a login page

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../style/NavBar.css"
+
+export default function NavBar() {
+    return (
+        <div className="navbar">
+            <a href="../index.tsx">Sign Up</a>
+            <a href="../index.tsx">Log In</a>
+        </div>
+    );
+}
