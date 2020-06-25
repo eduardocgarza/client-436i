257236fd7edc3c179533a38a2c9f54057b1f1745
Eduardo Garza <eduardo@garza.ca>
2020-06-25 15:48:11 -0700

    setup spotify login

--- src/components/Account/Profile/Profile.tsx
+++ src/components/Account/Profile/Profile.tsx
@@ -1,13 +1,18 @@
 import React from "react"
+import Spotify from "../Spotify/Spotify"
 // import ProfileHeader from "./ProfileHeader"
 // import ProfileBody from "./ProfileContent"
 
 export default function Profile() {
   return (
     <div className="profile-main">
-      {/* <ProfileHeader />
-      <ProfileBody /> */}
-      Profile
+
+      <hr/>
+      
+      <Spotify />
+      
+      <hr/>
+      
     </div>
   )
 }
