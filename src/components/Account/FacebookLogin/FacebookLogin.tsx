be72fba1dfd49ee5a52a3d678817b3608a0b625a
Eduardo Garza <eduardo@garza.ca>
2020-06-27 21:58:33 -0700

    added facebook react btn

---
+++ src/components/Account/FacebookLogin/FacebookLogin.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+import { Button } from "react-bootstrap"
+
+export default function FacebookLogin () {
+
+  function handleFacebook () {
+    
+  }
+  
+  return (
+    <div>
+      <Button variant="primary" onClick={handleFacebook}>
+        Login in with Facebook
+      </Button>
+    </div>
+  )
+}
--- src/components/Account/Profile/Profile.tsx
+++ src/components/Account/Profile/Profile.tsx
@@ -1,13 +1,14 @@
 import React from "react"
+import FacebookLogin from "../FacebookLogin/FacebookLogin"
 // import ProfileHeader from "./ProfileHeader"
 // import ProfileBody from "./ProfileContent"
 
 export default function Profile() {
   return (
     <div className="profile-main">
-      {/* <ProfileHeader />
-      <ProfileBody /> */}
-      Profile
+
+      <FacebookLogin />
+      
     </div>
   )
 }
