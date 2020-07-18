eba6d12e67c7ca4697c5ca3d7c435344516f10c4
ating23 <allantingca@live.ca>
2020-07-17 23:12:31 -0700

    spotify profile render done

--- src/components/AccountModule/Profile/Profile.tsx
+++ src/components/AccountModule/Profile/Profile.tsx
@@ -6,6 +6,7 @@ import useSessionContext from "../../../state/context/SessionContext"
 import useAppContext from "../../../state/context/ApplicationContext"
 import { Card, Container, Row, Col, Image, ListGroup, CardDeck } from "react-bootstrap"
 
+
 export default function Profile() {
   const { api } = useSessionContext()
   const { account, setAccount } = useAppContext()
@@ -22,20 +23,51 @@ export default function Profile() {
     fetchProfileData()
   }, [])
 
+  function renderCard(item) {
+    return <Card style={{width: '15%'}} key={item._id}>
+      <Card.Img variant="top" src={item.image.url}/>
+      <Card.Body>
+        <Card.Title>{item.name}</Card.Title>
+      </Card.Body>
+    </Card>
+  }
+  
+  function spotifyWrapper() {
+    return <Card key='spotify' style={{ width: '100%', padding:'2em'}}>
+      <Card.Title>Spotify</Card.Title>
+      <hr/>
+      <Card.Subtitle className='text-muted'> Top Artists </Card.Subtitle>
+      
+      {spotifyArtistWrapper()}
+      <Card.Subtitle className='text-muted'> Top Tracks </Card.Subtitle>
+      {spotifyTrackWrapper()}
+    </Card>
+  }
+
+  function spotifyArtistWrapper() {
+    return <div>
+      <CardDeck style={{width: '100%'}}>
+        {renderTopArtists()}
+      </CardDeck>
+      <hr/>
+    </div>
+  }
+
+  function spotifyTrackWrapper() {
+    return <div>
+      <CardDeck style={{width: '100%'}}>
+        {renderTopTracks()}
+        </CardDeck>
+    </div>
+  }
+
   function renderTopArtists() {
     let limit = 5;
     let counter = 0;
     return account.spotify.artists.map(artist => {
       while(counter<limit) {
         counter++
-        return <div>
-          <Card key={artist._id}>
-            <Card.Img variant="top" src={artist.image.url}/>
-            <Card.Body>
-              <Card.Title>{artist.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        </div>
+        return renderCard(artist)
       }
     })
   }
@@ -46,14 +78,7 @@ export default function Profile() {
     return account.spotify.tracks.map(track => {
       while(counter<limit) {
         counter++
-        return <div>
-          <Card key={track._id}>
-            <Card.Img variant="top" src={track.image.url}/>
-            <Card.Body>
-              <Card.Title>{track.name}</Card.Title>
-            </Card.Body>
-          </Card>
-        </div>
+        return renderCard(track)
       }
     })
   }
@@ -61,27 +86,22 @@ export default function Profile() {
   return (
     <div className="profile-main">
       <hr/>
-      <Card border="primary" style={{ width: '100%', padding:'2em'}}>
+      <Card key='profile' border="primary" style={{ width: '100%', padding:'2em'}}>
         <Container>
           <Card.Img variant="top" src={account ? account.spotify.image.url: null} />
           <Card.Body>
             <Card.Title>{account ? account.name: null}</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the bulk
-              of the card's content.
-            </Card.Text>
-            <ListGroup variant="flush">
-              <ListGroup.Item>Cras justo odio</ListGroup.Item>
-              <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-              <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
-            </ListGroup>
+              <ListGroup variant="flush">
+                <ListGroup.Item> Cras justo odio</ListGroup.Item>
+                <ListGroup.Item> Dapibus ac facilisis in</ListGroup.Item>
+                <ListGroup.Item> Vestibulum at eros</ListGroup.Item>
+              </ListGroup>   
           </Card.Body>
         </Container>        
       </Card>
-      
       <hr/>
-      { account ? renderTopArtists(): null}
-      { account ? renderTopTracks(): null }
+      { account ? account.spotifyVerified ? spotifyWrapper(): null: null}
+      <hr/>
     </div>
   )
 }
