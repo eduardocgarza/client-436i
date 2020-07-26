74dd28a34162df6f55e5162b61555003d1f83ade
Eduardo Garza <eduardo@garza.ca>
2020-07-25 20:19:48 -0700

    modified unauth pages + added error message on login+signup

--- src/components/PublicModule/Home/Home.tsx
+++ src/components/PublicModule/Home/Home.tsx
@@ -3,11 +3,14 @@ import { PageWrapper } from "../../../assets/styles/PageWrapper"
 import homeJumbotron from "../../../assets/images/home.jpg"
 import PageHeader from "../../_Shared/PageHeader/PageHeader"
 import { HomeJumbotron } from "./HomeStyles"
+import { HeaderTwo, ParagraphItem } from "../../_Shared/Layout/Layout"
 
 export default function Home () {
   return (
     <PageWrapper>
       <PageHeader text="Welcome to educonnections" />
+      <HeaderTwo text="educonnections" />
+      <ParagraphItem text="educonnections is a social media platform that matches you with other students at your university based on common artists, classes, likes, and songs. Our platform helps students meet online by matching with each other based on common Facebook likes, Spotify artists and songs, and UBC classes."/>
       <HomeJumbotron src={homeJumbotron} />
     </PageWrapper>
   )
